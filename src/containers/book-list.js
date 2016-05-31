94952af3af04bf39f8adfbb61db81067e5e161f2
Max Norton <maxnorton.nz@gmail.com>
2016-05-31 18:21:40 +1000

    complete section 4

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
@@ -26,4 +33,11 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anythign returned from this function will end up as props on the booklist container
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, the result should be passed to all our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+// promote booklist from a component to a container - ot needs to know about this new dispatch method, selectBook. Make it availavle as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'javascript good parts'},
-    { title: 'harry pothead'},
-    { title: 'dark tower'},
-    { title: 'eloquent'}
+    { title: 'javascript good parts', pages: 44 },
+    { title: 'harry pothead', pages: 534 },
+    { title: 'dark tower', pages: 34 },
+    { title: 'eloquent', pages: 9 }
   ]
 }
